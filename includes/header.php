be4dbc05fb368ba322556fe52081fbd29b258c9d
MichaellAlavedraMunayco <f.michaell.a.m@gmail.com>
2019-08-09 19:54:50 -0500

    widget jsd

--- includes/header.php
+++ includes/header.php
@@ -12,8 +12,8 @@
 </head>
 
 <body>
-  <!-- <script data-jsd-embedded data-key="247bc2c3-8c1a-4fc0-91eb-4b5eea808a9e"
-    data-base-url="https://jsd-widget.atlassian.com" src="https://jsd-widget.atlassian.com/assets/embed.js"></script> -->
+  <script data-jsd-embedded data-key="247bc2c3-8c1a-4fc0-91eb-4b5eea808a9e"
+    data-base-url="https://jsd-widget.atlassian.com" src="https://jsd-widget.atlassian.com/assets/embed.js"></script>
   <nav class="navbar navbar-dark bg-dark p-0">
     <div class="container">
       <a href="index.php" class="navbar-brand"><i class="fas fa-calculator"></i> BSC</a>
